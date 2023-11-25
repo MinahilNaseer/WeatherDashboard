f1cb4f3e57ef4321114601e4d32554a1e4a4c291
hamnakashif <hamnakashif397@gmail.com>
2023-11-25 21:06:14 +0500

    initial commit

--- server/app.js
+++ server/app.js
@@ -1,9 +1,40 @@
-const express = require('express');
+//const express = require('express');
 
+//const app = express();
+
+//app.use(express.static('public'));
+
+//app.listen(3000 ,() => {
+  //  console.log("Server is running on port 3000");
+//})
+const express = require("express");
 const app = express();
 
-app.use(express.static('public'));
+// Set the view engine to EJS
+app.set("view engine", "ejs");
 
-app.listen(3000 ,() => {
-    console.log("Server is running on port 3000");
-})
+// Serve the public folder as static files
+app.use(express.static("public"));
+
+// Render the index template with default values for weather and error
+app.get("/", (req, res) => {
+  res.render("index", { weather: null, error: null });
+});
+
+// Handle the /weather route
+app.get("/weather", async(req, res) => {
+  // Get the city from the query parameters
+  const city = req.query.city;
+  const apikey="094b545dde613af5667ba10639a224f8";
+
+  // Add your logic here to fetch weather data from the API
+  const url="https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apikey}";
+  // Render the index template with the weather data and error message
+  res.render("index", { weather: null, error: null });
+});
+
+// Start the server and listen on port 3000 or the value of the PORT environment variable
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App is running on port ${port}`);
+});
