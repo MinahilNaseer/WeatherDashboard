6b23d5ffd7ddaababa786cfdcea035b0e66d9dee
hamnakashif <hamnakashif397@gmail.com>
2023-11-26 15:52:52 +0500

    modifying search functionality and the api usage code

--- server/app.js
+++ server/app.js
@@ -7,34 +7,23 @@
 //app.listen(3000 ,() => {
   //  console.log("Server is running on port 3000");
 //})
-const express = require("express");
+const express = require('express');
+const request = require('request');
+
 const app = express();
 
-// Set the view engine to EJS
-app.set("view engine", "ejs");
-
-// Serve the public folder as static files
-app.use(express.static("public"));
-
-// Render the index template with default values for weather and error
-app.get("/", (req, res) => {
-  res.render("index", { weather: null, error: null });
+app.get('/forecast', (req, res) => {
+	let city = req.query.city;
+	var request = require('request');
+	request(
+		`https://samples.openweathermap.org/data/2.5/forecast?q=${city}&appid=193ab6f4762de43d221a6c2ab3e315eb`,
+		function(error, response, body) {
+			let data = JSON.parse(body);
+			if (response.statusCode === 200) {
+				res.send(`The weather in your city "${city}" is ${data.list[0].weather[0].description}`);
+			}
+		}
+	);
 });
 
-// Handle the /weather route
-app.get("/weather", async(req, res) => {
-  // Get the city from the query parameters
-  const city = req.query.city;
-  const apikey="094b545dde613af5667ba10639a224f8";
-
-  // Add your logic here to fetch weather data from the API
-  const url="https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apikey}";
-  // Render the index template with the weather data and error message
-  res.render("index", { weather: null, error: null });
-});
-
-// Start the server and listen on port 3000 or the value of the PORT environment variable
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`App is running on port ${port}`);
-});
+app.listen(3000, () => console.log('Server started on port 3000'));
