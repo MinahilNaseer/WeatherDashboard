c6caf23a333ef008979eb905eb0629878a7a4e05
hamnakashif <hamnakashif397@gmail.com>
2023-12-02 15:25:45 +0500

    real time date and day added and resolution of the issue of api running on edge

--- server/public/main.js
+++ server/public/main.js
@@ -1,3 +1,16 @@
+var today=new Date();
+var day=today.getDay();
+var daylist=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
+document.querySelector(".day").innerHTML=daylist[day]
+console.log(daylist[day]);
+
+const year = today.getFullYear();
+const month = today.getMonth() + 1; // Months are zero-based, so add 1
+const date = today.getDate();
+const current  = `${year}-${month < 10 ? '0' : ''}${month}-${date < 10 ? '0' : ''}${date}`;
+document.getElementById('date').textContent = current;
+console.log(current);
+
 $(document).ready(function(){
     $('#weatherForm').submit(function (e){
         e.preventDefault();
