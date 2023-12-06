a84ddd1fb1b965e76c193b2cf66e20d787d4f07e
MinahilNaseer <01-131212-018@student.bahria.edu.pk>
2023-12-06 17:36:26 +0500

    Fully created login and signup page

--- server/public/main.js
+++ server/public/main.js
@@ -26,7 +26,20 @@ $(document).ready(function(){
         });
     });
 });
-function redirectToLoginPage() {
-    window.location.href = "login.html";
-}
-    
+
+const showPopupBtn = document.querySelector(".login-btn");
+const hidePopupBtn = document.querySelector(".form-popup .close-btn");
+const loginSignupLink = document.querySelectorAll(".form-box .bottom-link a");
+const formPopup = document.querySelector(".form-popup");
+
+showPopupBtn.addEventListener("click", () => {
+    document.body.classList.toggle("show-popup");
+});
+hidePopupBtn.addEventListener("click", () => showPopupBtn.click());
+
+loginSignupLink.forEach(link => {
+    link.addEventListener("click", (e)=> {
+        e.preventDefault();
+        formPopup.classList[link.id === "signup-link" ? 'add' : 'remove']("show-signup")
+    })
+})
