8feb9d9311e83a0b80935549056f04106a0655a0
MinahilNaseer <01-131212-018@student.bahria.edu.pk>
2023-12-08 20:39:18 +0500

    Adding a current location button

--- server/public/main.js
+++ server/public/main.js
@@ -12,11 +12,7 @@ document.getElementById('date').textContent = current;
 console.log(current);
 
 $(document).ready(function(){
-    $('#weatherForm').submit(function (e){
-        e.preventDefault();
-
-        const cityName = $('#CityInput').val();
-
+    function getWeather(cityName){
         $.post('/',{cityName},function(data){
             console.log(data);
             $('#weather-temp').text(data.temp+'°C');
@@ -24,6 +20,37 @@ $(document).ready(function(){
             $('#name').text(data.name);
             
         });
+    }
+    function getWeatherByLocation() {
+        if ('geolocation' in navigator) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                const { latitude, longitude } = position.coords;
+                const roundedLatitude = latitude.toFixed(4);
+                const roundedLongitude = longitude.toFixed(4);
+                console.log('Latitude:', roundedLatitude);
+                console.log('Longitude:', roundedLongitude);
+                $.post('/', { lat: latitude, lon: longitude }, function(data) {
+                    console.log(data);
+                    $('#weather-temp').text(data.temp + '°C');
+                    $('#weather-desc').text(data.desc);
+                    $('#name').text(data.name);
+                });
+            }, function(error) {
+                console.error('Error getting geolocation:', error);
+            });
+        } else {
+            console.error('Geolocation is not supported by your browser.');
+        }
+    }
+
+    $('#weatherForm').submit(function (e){
+        e.preventDefault();
+        const cityName = $('#CityInput').val();
+        getWeather(cityName);
     });
+    $('#getLocationBtn').click(function() {
+        getWeatherByLocation();
+    });
+
 });
     
