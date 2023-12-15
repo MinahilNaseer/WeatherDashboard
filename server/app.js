d57b43e755cff718821f49f0be5a0579eb563076
hamnakashif <hamnakashif397@gmail.com>
2023-12-15 23:43:34 +0500

    exception handling

--- server/app.js
+++ server/app.js
@@ -55,23 +55,11 @@ app.get('/',(req,res)=>{
 })
 app.post('/',(req,res)=>{
   //console.log(req.body.cityName);
-  const { cityName, lat, lon } = req.body;
-  let query;
-
-  if (cityName) {
-      query = cityName;
-      const apiKey = '9a45dc5470304ed7d813a404d98c2758';
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${apiKey}&units=metric" ;
-
-  https.get(url, (response) => {
-      let data = '';
-      response.on('data', (chunk) => {
-          data += chunk;
-          const weatherData = JSON.parse(data);
-      console.log(weatherData);
-      const temp = weatherData.main.temp;
-      console.log(temp);
-      });
+  const query = req.body.cityName
+  const apiKey = '9a45dc5470304ed7d813a404d98c2758'
+  const url = 'https://api.openweathermap.org/data/2.5/weather?q='+query+'&appid='+apiKey+'&units=metric'
+  https.get(url,(response)=>{
+    let data= '';
     response.on('data',(chunk)=>{
       //console.log(data);
       
@@ -81,80 +69,32 @@ app.post('/',(req,res)=>{
       //console.log(temp);
       data +=chunk;
     })
-    response.on('end', () => {
-      try {
-          const weatherData = JSON.parse(data);
-          if(response.statusCode === 200){
-            const temp = weatherData.main.temp;
-          const name = weatherData.name;
-          const desc = weatherData.weather[0].description;
-          res.json({
-              temp,
-              desc,
-              name
-          });
-          }else {
-            // If the required properties are not present, handle the error
-            console.error('Error: Unexpected response from OpenWeatherMap API');
-                console.error('Status Code:', response.statusCode);
-                console.error('Response:', data);
-                res.status(response.statusCode || 500).json({ error: 'Internal Server Error' });
-        }
-      } catch (error) {
-          console.error('Error parsing weather data:', error);
-          res.status(500).json({ error: 'Internal Server Error' });
-      }
-  });
-});
-} else if (lat && lon) {
-
-const latitude = lat; // Replace with the actual latitude
-const longitude = lon;
-//query = lat=${lat}&lon=${lon};
-const lang = 'en';
-const apiKey1 = '9a45dc5470304ed7d813a404d98c2758';
-const url1 = "https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey1}&lang=${lang}";
-https.get(url1, (response) => {
-  let data = '';
-  response.on('data', (chunk) => {
-      data += chunk;
+    response.on('end',()=>{
       const weatherData = JSON.parse(data);
-  console.log(weatherData);
-  const temp = weatherData.main.temp;
-  console.log(temp);
-  });
-  response.on('end', () => {
-      try {
-          const weatherData = JSON.parse(data);
-          if(response.statusCode === 200){
-            const temp = weatherData.main.temp;
-          const name = weatherData.name;
-          const desc = weatherData.weather[0].description;
-          res.json({
-              temp,
-              desc,
-              name
-          });
-          }else {
-            // If the required properties are not present, handle the error
-            console.error('Error: Unexpected response from OpenWeatherMap API');
-                console.error('Status Code:', response.statusCode);
-                console.error('Response:', data);
-                res.status(response.statusCode || 500).json({ error: 'Internal Server Error' });
-        }
-      } catch (error) {
-          console.error('Error parsing weather data:', error);
-          res.status(500).json({ error: 'Internal Server Error' });
-      }
-  });
-});
-} else {
-  res.status(400).json({ error: 'City name or geolocation is required.' });
-  return; // Add a return statement to exit the function here
-}
+      console.log('Wind Direction:', weatherData.main.temp);
+      console.log(weatherData);
 
-// Now you can use query outside the if block
+      const temp = Math.round(weatherData.main.temp);
+      const name = weatherData.name;
+      const desc =weatherData.weather[0].description;      const pressure=weatherData.main.pressure;
+      const humidity=weatherData.main.humidity;
+      const windspeed= weatherData.wind.speed;
+      const sunrise = weatherData.sys && weatherData.sys.sunrise !== undefined ? formatTime(weatherData.sys.sunrise) : 'N/A';
+      const sunset=weatherData.sys&&weatherData.sys.sunser!==undefined?formatTime(weatherData.sys.sunset):'N/A';
+       res.json({
+        temp,
+        desc,
+        name,
+        visibility,
+        pressure,
+        humidity,
+        windspeed,
+        sunrise,
+        });
+   
+    });
 
+  });
 })
 
 app.listen(3000,()=> console.log("our server is running"))
