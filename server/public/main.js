e4b8f117182d21b00381560e160b68b15eadd126
MinahilNaseer <01-131212-018@student.bahria.edu.pk>
2023-12-17 16:52:42 +0500

    Resolving issues

--- server/public/main.js
+++ server/public/main.js
@@ -12,31 +12,47 @@ const current  = `${year}-${month < 10 ? '0' : ''}${month}-${date < 10 ? '0' : '
 document.getElementById('date').textContent = current;
 console.log(current);
 
-
-
-// for city and temprature
 $(document).ready(function(){
-    $('#weatherForm').submit(function (e){
-        e.preventDefault();
-
-        const cityName = $('#CityInput').val();
-
+    function getWeather(cityName){
         $.post('/',{cityName},function(data){
             console.log(data);
-           $('#weather-temp').text(data.temp+'C');
-           $('#weather-desc').text(data.desc);
+            $('#weather-temp').text(data.temp+'°C');
+            $('#weather-desc').text(data.desc);
             $('#name').text(data.name);
-            $('#pressure').text(data.press + ' hPa');
-            //const iconUrl = `https://openweathermap.org/img/w/${data.icon}.png`;
-            //$('#weather-icon').text(data.icon);
-            const iconUrl = `https://openweathermap.org/img/w/01d.png`;
-            $('#weather-icon').attr('src', iconUrl);
-            $('#weather-icon').attr('alt', data.desc);
-            $('#windspeed').text(data.windspeed+'Km/h');  
-            $('#humidity').text(data.humidity+'%');
+            
         });
+    }
+    function getWeatherByLocation() {
+        if ('geolocation' in navigator) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                const { latitude, longitude } = position.coords;
+                const roundedLatitude = latitude.toFixed(4);
+                const roundedLongitude = longitude.toFixed(4);
+                console.log('Latitude:', roundedLatitude);
+                console.log('Longitude:', roundedLongitude);
+                $.post('/', { lat: latitude, lon: longitude }, function(data) {
+                    console.log(data);
+                    $('#weather-temp').text(data.temp + '°C');
+                    $('#weather-desc').text(data.desc);
+                    $('#name').text(data.name);
+                });
+            }, function(error) {
+                console.error('Error getting geolocation:', error);
+            });
+        } else {
+            console.error('Geolocation is not supported by your browser.');
+        }
+    }
 
+    $('#weatherForm').submit(function (e){
+        e.preventDefault();
+        const cityName = $('#CityInput').val();
+        getWeather(cityName);
     });
+    $('#getLocationBtn').click(function() {
+        getWeatherByLocation();
+    });
+
 });
 
 const showPopupBtn = document.querySelector(".login-btn");
