be01fdeac9b094bce24e0b04945f0756243a7a56
hamnakashif <hamnakashif397@gmail.com>
2023-12-30 14:41:35 +0500

    resolving issues

--- server/app.js
+++ server/app.js
@@ -71,30 +71,25 @@ app.post('/',(req,res)=>{
     })
     response.on('end',()=>{
       const weatherData = JSON.parse(data);
-      console.log('Wind Direction:', weatherData.main.temp);
       console.log(weatherData);
-
       const temp = Math.round(weatherData.main.temp);
       const name = weatherData.name;
-      const desc =weatherData.weather[0].description;      const pressure=weatherData.main.pressure;
+      const desc =weatherData.weather[0].description;     
+       const pressure=weatherData.main.pressure;
       const humidity=weatherData.main.humidity;
       const windspeed= weatherData.wind.speed;
-      const sunrise = weatherData.sys && weatherData.sys.sunrise !== undefined ? formatTime(weatherData.sys.sunrise) : 'N/A';
-      const sunset=weatherData.sys&&weatherData.sys.sunser!==undefined?formatTime(weatherData.sys.sunset):'N/A';
+     const feels_like=weatherData.main.feels_like;
        res.json({
         temp,
         desc,
         name,
-        visibility,
         pressure,
         humidity,
         windspeed,
-        sunrise,
+        feels_like
         });
    
     });
-
-  });
-})
-
-app.listen(3000,()=> console.log("our server is running"))
+});
+});
+app.listen(3000, () => console.log("Our server is running"));
